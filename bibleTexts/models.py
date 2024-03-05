4309b406331141bfa1b53f74715f63dc22de589a
django <thiago_tawan@outlook.com>
2024-03-05 18:48:05 -0300

    ..

--- bibleTexts/models.py
+++ bibleTexts/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 class TextsDevotional(models.Model):
     title = models.CharField(max_length=200) 
+    summary = models.CharField(max_length=400)
     content = models.TextField()
     version = models.CharField(max_length=10)
+    chapter = models.CharField(max_length=30)
     concluded = models.BooleanField(default=False) 
